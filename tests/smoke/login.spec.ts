808e9f8c4bb565ff3fcf45b3de2269a3e2a698c3
Mateusz <krzeszowiecm1@gmail.com>
2024-05-09 14:49:45 +0200

    chore: test data for user mail

--- tests/smoke/login.spec.ts
+++ tests/smoke/login.spec.ts
@@ -1,12 +1,13 @@
 import { LoginPage } from '../../src/pages/login.page';
 import { WelcomePage } from '../../src/pages/welcome.page';
+import { testUser1 } from '../../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
   test('login with correct credentials @GAD-R02-01', async ({ page }) => {
     //Arrange
-    const userEmail = '';
-    const userPassword = '';
+    const userEmail = testUser1.userEmail;
+    const userPassword = 'test1';
     const loginPage = new LoginPage(page);
     const welcomePage = new WelcomePage(page);
 
